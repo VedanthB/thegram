3cb8687f9ca20df0d04dae6190a3b4993d91b5e6
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-18 02:49:10 +0530

    login page

--- gram/src/pages/login.js
+++ gram/src/pages/login.js
@@ -1,3 +1,21 @@
+import { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
 export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - OG Gram';
+  }, []);
   return <p>I am the login page</p>;
 }
