813f7a045969700a5d5faf459cd8ebaefc7f5dd4
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-18 01:56:44 +0530

    yarn add react-router-dom

--- gram/src/App.js
+++ gram/src/App.js
@@ -1,5 +1,19 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// eslint-disable-next-line import/no-unresolved
+const Login = lazy(() => import('./pages/login'));
+
 function App() {
-  return <p>hello</p>;
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading......</p>}>
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
