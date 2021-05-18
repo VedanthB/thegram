7b40b92c87a6f08b89400134635140db77c13882
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-18 18:39:48 +0530

    Continue working on Login Page (Login Form)

--- gram/src/pages/login.js
+++ gram/src/pages/login.js
@@ -1,5 +1,6 @@
+/* eslint-disable prettier/prettier */
 import { useContext, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 
 export default function Login() {
@@ -19,7 +20,60 @@ export default function Login() {
   }, []);
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <p>I am the login page</p>
+      <div className='flex w-3/5'>
+          <img src='/images/iphone-with-profile.jpg' alt='iphone with the og gram app' />
+      </div>
+
+      <div className='flex w-2/5 flex-col'>
+         <div className='flex flex-col items-center bg-white p-4 border border-grey-primary mb-4 '>
+          <h1 className='flex justify-center w-full'>
+              <img src='/images/logo.png' alt='logo' className='mt-2 w-6/12 mb-4 ' />
+          </h1>
+
+          {error && <p className='mb-4 text-xs text-red-primary'>{error}</p> }
+
+          <form onSubmit={handleLogin} method='POST'>
+              <input
+               aria-label='Enter your email address'
+               type='text'
+               placeholder='Email address'
+               onChange={({target}) => setEmailAddress(target.value) }
+               className='text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-grey-primary outline-none rounded-full mb-2'
+               />
+
+              <input
+               aria-label='Enter your email password'
+               type='password'
+               placeholder='Password'
+               onChange={({target}) => setPassword(target.value) }
+               className='text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-grey-primary outline-none rounded-full mb-2'
+               />
+
+               <button
+               disabled={isInvalid}
+               type='submit'
+               className={`bg-blue-500 w-full text-white  rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}
+               >
+                Log In
+               </button>
+          </form>
+        </div>
+
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-grey-primary" >
+            <p>Dont have an Account? {` `}
+              <Link to='/signup' className='font-bold text-blue-medium'>
+                  Sign up
+              </Link>
+             </p>
+        </div>
+      </div>
     </div>
   );
 }
+
+ // TODO
+   
+// text-red-primary -> hex value
+// text-grey-base  -> hex value
+// border-grey-primary -> hex value
+// bg-blue-medium -> hex value
