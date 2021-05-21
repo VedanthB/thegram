7284e256406c5a1e22c8f415489f3e48e7cf6241
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-22 03:39:34 +0530

    protected route

--- gram/src/App.js
+++ gram/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import UserContext from './context/user';
 import useAuthListener from './hooks/use-auth-listener';
+import ProtectedRoute from './helpers/protected.route';
 
 // eslint-disable-next-line import/no-unresolved
 const Login = lazy(() => import('./pages/login'));
@@ -20,7 +21,9 @@ function App() {
           <Switch>
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGN_UP} component={SignUp} />
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <ProtectedRoute path={ROUTES.DASHBOARD} user={user} exact>
+              <Dashboard />
+            </ProtectedRoute>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
